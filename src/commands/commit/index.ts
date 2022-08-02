7799e897a075ef4c02476bcdce223cf2485bc7fb
kanekotic <me@kanekotic.com>
2022-08-02 22:45:14 +0200

    Alvaro | added basic questions

--- src/commands/commit/index.ts
+++ src/commands/commit/index.ts
@@ -11,16 +11,40 @@ hello friend from oclif! (./src/commands/hello/index.ts)
   ]
 
   async run(): Promise<void> {
+// : 
+// : 
+// : 
+// : 
+// : 
     const questions: any = [
       {
-        type: 'text',
-        name: 'dish',
-        message: 'Do you like pizza?'
+        type: 'select',
+        name: 'type',
+        message: 'Pick a color',
+        choices: [
+          { title: 'feat', description: 'A new feature', value: 'feat' },
+          { title: 'fix', description: 'A bug fix', value: 'fix' },
+          { title: 'docs', description: 'Documentation only changes', value: 'docs' },
+          { title: 'style', description: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)', value: 'style' },
+          { title: 'refactor', description: 'A code change that neither fixes a bug nor adds a feature', value: 'refactor' },
+          { title: 'perf', description: 'A code change that improves performance', value: 'perf' },
+          { title: 'test', description: 'Adding missing or correcting existing tests', value: 'test' },
+          { title: 'chore', description: 'Changes to the build process or auxiliary tools and libraries such as documentation generation', value: 'chore' },
+        ],
+        initial: 0
       },
       {
-        type: (prev: any) => prev == 'pizza' ? 'text' : null,
-        name: 'topping',
-        message: 'Name a topping'
+        type: (prev: any) => ['feat', 'refactor'].includes(prev) ? 'toggle' : null,
+        name: 'breaking',
+        message: 'Is it a breaking chamge?',
+        initial: false,
+        active: 'yes',
+        inactive: 'no'
+      },
+      {
+        type: 'text',
+        name: 'message',
+        message: `What's the commit message?`
       }
     ];
     const response = await prompts(questions);
